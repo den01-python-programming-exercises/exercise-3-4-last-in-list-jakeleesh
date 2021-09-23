c5d33154136fac5db9b476f64c35fd46a3f4868a
Jake Lee <jake.lee@fdmgroup.com>
2021-09-23 22:19:22 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,13 @@
 def main():
     #write your code below this line
+    names = []
+    while True:
+        name = input()
+        if (name == ""):
+            break
+        else:
+            names.append(name)
+    print(names[len(names) - 1])
 
 if __name__ == '__main__':
     main()
